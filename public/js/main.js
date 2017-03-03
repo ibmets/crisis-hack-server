fd6036778c6c2cfddeb1556d3405feabe423be9a
Dan Cunnington <dancunnington@uk.ibm.com>
2017-03-03 14:05:03 +0000

    drag ebola crisis onto map

--- public/js/main.js
+++ public/js/main.js
@@ -5,85 +5,53 @@ $(document).ready(function() {
 
 	});
 
-	/*
-	var mymap = L.map('mapid').setView([8.4480594, -12.908401], 13);
-
-	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-		maxZoom: 18,
+	
+	var mymap = L.map('mapid').setView([8.4562, -13.2277], 13);
+	var group = new L.featureGroup();
+	mymap.addLayer(group);
+	var tileURL = "https://api.mapbox.com/styles/v1/dancunnington/ciztrtban00vh2sqjn5hci14f/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZGFuY3VubmluZ3RvbiIsImEiOiI1M2RiZTJlNTFkYWZiNjczNWRjODEwOGNlNzkzMTBlZiJ9.eAfl4KKi9ZRASZgxL8KcYw";
+	L.tileLayer(tileURL, {
+		// L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 20,
 		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
 			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
 			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
 		id: 'mapbox.streets'
 	}).addTo(mymap);
 
-	var marker = L.marker([8.4480594, -12.908401]).addTo(mymap);
+	var ebolaDrag = false;
+	$("#ebola").on('dragstart', function(e) {
+		ebolaDrag = true;
+	});
 
-	var circle = L.circle([8.4480594, -12.908401], {
-	    color: 'red',
-	    fillColor: '#f03',
-	    fillOpacity: 0.5,
-	    radius: 500
-	}).addTo(mymap);
-
-
-
-
-	var options = {
-	  uri: 'http://nr-crisishack.eu-gb.mybluemix.net/ce-to-geojson?concept=community%20care%20centre',
-	  json: true
-	};
-
-	var p = new Promise(function(resolve, reject) {
-	  var xhr = new XMLHttpRequest();
-
-
-		if ("withCredentials" in xhr) {
-
-			// Check if the XMLHttpRequest object has a "withCredentials" property.
-			// "withCredentials" only exists on XMLHTTPRequest2 objects.
-		  xhr.open('GET', options.uri);
-
-		} else if (typeof XDomainRequest != "undefined") {
-
-			// Otherwise, check if XDomainRequest.
-			// XDomainRequest only exists in IE, and is IE's way of making CORS requests.
-			xhr = new XDomainRequest();
-			xhr.open('GET', options.uri);
-
-		} else {
-
-			// Otherwise, CORS is not supported by the browser.
-			xhr = null;
-
+	mymap.on('mouseup', function(e) {
+		//if ebola crisis has been dragged
+		if (ebolaDrag) {
+			ebolaDrag = false;
+			addPeopleToMap(e.latlng.lat, e.latlng.lng, 1000);
 		}
-
-	  xhr.onload = function() {
-	    if (xhr.status == 200) {
-	      resolve(xhr.response);
-	    }
-	    else {
-	      reject(Error(xhr.statusText));
-	    }
-	  };
-	  // Handle network errors
-	  xhr.onerror = function() {
-	    reject(Error("Network Error"));
-	  };
-
-	  // Make the request
-	  xhr.send();
 	});
 
-	p.then(function(response) {
+	function addPeopleToMap(latitude, longitude, radius) {
+		group.clearLayers();
+		$.get('/geo/peopleNearLocation/'+latitude+'/'+longitude+'/'+radius, function(people) {
+			for (var i=0; i<people.length; i++) {
+				//add marker to map
+				var lat = people[i].geometry.coordinates[1];
+				var lng = people[i].geometry.coordinates[0];
 
-		var heatmapArray = [];
-		console.log(response);
+				L.marker([lat,lng]).addTo(group);
 
-		var heat = L.heatLayer([
-		    [8.4480594, -12.908401, 0.8], // lat, lng, intensity
-		], {radius: 25}).addTo(mymap);
-
-	});
-	*/
+			}
+			var circle = L.circle([latitude, longitude], {
+			    color: 'red',
+			    fillColor: '#f03',
+			    fillOpacity: 0.5,
+			    radius: radius
+			}).addTo(group);
+			mymap.fitBounds(group.getBounds());
+		});
+	}
+	
 
 });
